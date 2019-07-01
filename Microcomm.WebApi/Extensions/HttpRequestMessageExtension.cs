09f4d9dbbbeb664dec005f6337a081a95df48849
nwilliamfeng <nwilliamfeng@163.com>
2019-07-01 17:54:01 +0800

    增加ip过滤

---
+++ Microcomm.WebApi/Extensions/HttpRequestMessageExtension.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Microcomm.Web.Http
+{
+    public static class HttpRequestMessageExtension
+    {
+        /// <summary>
+        /// 返回请求对应客户端的IpV6地址
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        public static string GetIP(this HttpRequestMessage requestMessage)
+        {        
+            //// Owin Hosting
+            //if (requestMessage.Properties.ContainsKey("MS_OwinContext"))
+            //{
+            //    return HttpContext.Current != null
+            //        ? HttpContext.Current.Request.GetOwinContext().Request.RemoteIpAddress
+            //        : null;
+            //}
+            // Web Hosting
+            if (requestMessage.Properties.ContainsKey("MS_HttpContext"))
+            {
+                return HttpContext.Current != null ? HttpContext.Current.Request.UserHostAddress : null;
+            }
+            // Self Hosting
+            if (requestMessage.Properties.ContainsKey(RemoteEndpointMessageProperty.Name))
+            {
+                RemoteEndpointMessageProperty property =(RemoteEndpointMessageProperty)requestMessage.Properties[RemoteEndpointMessageProperty.Name];
+                return property != null ? property.Address : null;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 返回对应请求的所有ipv4地址
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetIPV4AddressList(this HttpRequestMessage requestMessage)
+        {
+            Func<string, IEnumerable<string>> func = addr =>
+              {
+                  if (addr == null)
+                      return new string[] { };
+                  IPHostEntry hostInfo = Dns.GetHostEntry(addr);
+                  return hostInfo.AddressList.Where(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).Select(x => x.MapToIPv4().ToString());
+              };
+            if (requestMessage.Properties.ContainsKey("MS_HttpContext"))
+            {
+                var addr = HttpContext.Current != null ? HttpContext.Current.Request.UserHostAddress : null;
+                return func(addr);
+            }
+ 
+            if (requestMessage.Properties.ContainsKey(RemoteEndpointMessageProperty.Name))
+            {
+                RemoteEndpointMessageProperty property = (RemoteEndpointMessageProperty)requestMessage.Properties[RemoteEndpointMessageProperty.Name];
+                var addr = property != null ? property.Address : null;
+                return func(addr);
+            }
+            return null;
+        }
+
+        public static bool AllowIP(this HttpRequestMessage requestMessage,IEnumerable<string> whiteIPs)
+        {
+            var ips = requestMessage.GetIPV4AddressList();
+            return whiteIPs.Any(x => ips.Any(ip=>ip.Equals(x,StringComparison.InvariantCultureIgnoreCase)));
+        }
+    }
+}
---
+++ Microcomm.WebApi/Handlers/IPFilterHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microcomm.Web.Http.Handlers
+{
+    /// <summary>
+    /// 客户端IP过滤
+    /// </summary>
+    public class IPFilterHandler:DelegatingHandler
+    {
+        public IEnumerable<string> WhiteIPs { get; set; }
+
+        public IPFilterHandler(IEnumerable<string> whiteIPs)
+        {
+            if (whiteIPs == null)
+                throw new ArgumentNullException("IP白名单为空。");
+            this.WhiteIPs = whiteIPs;
+        } 
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.AllowIP(this.WhiteIPs))
+            {
+                return await base.SendAsync(request, cancellationToken);
+            }
+            return request.CreateErrorResponse(HttpStatusCode.Unauthorized , "未授权的IP");
+        }
+    }
+}
